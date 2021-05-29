f1fe5277ee36ad445ead177397bd6abc4936f54a
Thanh Nguyen <nnct2000@gmail.com>
2021-05-29 11:30:55 +1000

    Print contents from state

--- BookStore/BookStore/ClientApp/src/components/Postboard.tsx
+++ BookStore/BookStore/ClientApp/src/components/Postboard.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import {ApplicationState} from '../store';
+import { connect, useSelector } from 'react-redux';
+import { ApplicationState } from '../store';
 import * as PostboardStore from '../store/Postboard';
-import FetchData from './FetchData';
+import PostLayout from './Post';
 
-type PostboardProps = PostboardStore.PostboardState 
+type PostboardProps = PostboardStore.PostboardState
     & typeof PostboardStore.actionCreators;
 
-const Postboard = (props:PostboardProps) => {
+const Postboard = (props: PostboardProps) => {
+    React.useEffect(() => {
+        props.requestPosts();
+    }, []);
+
+    console.log("Posts: " + props.posts);
+
     return (
-         <React.Fragment>
-            <h1>Posts</h1> 
+        <React.Fragment>
+            <h1>Posts</h1>
             <p>Anyway, check swagger</p>
-            {console.log("Posts: "+props.posts)}
+            {
+                props.posts.map(p => (PostLayout(p)))
+            }
         </React.Fragment>
     )
 }
 
 export default connect(
-    (state: ApplicationState) => state.postBoard,
+    (state: ApplicationState) => state.postboard,
     PostboardStore.actionCreators
 )(Postboard as any);
